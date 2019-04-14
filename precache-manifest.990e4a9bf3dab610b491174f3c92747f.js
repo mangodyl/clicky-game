d05f63031eed79bdd0c6d50d9a73f0fdb87dfb8d
mango dyl <dylanbonds@hotmail.com>
2019-04-14 12:59:37 -0500

    Updates

---
+++ precache-manifest.990e4a9bf3dab610b491174f3c92747f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "111aa50e7d021d7b7051",
+    "url": "/clicky-game/static/css/main.3288d3f5.chunk.css"
+  },
+  {
+    "revision": "111aa50e7d021d7b7051",
+    "url": "/clicky-game/static/js/main.e12b8869.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clicky-game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "933c71bfe26550c52ea0490ad43bbfad",
+    "url": "/clicky-game/static/media/iasip-charlie-look.933c71bf.png"
+  },
+  {
+    "revision": "efb0a9c3e3a7db282be5b98dec46ee05",
+    "url": "/clicky-game/static/media/iasip-charlie.efb0a9c3.jpg"
+  },
+  {
+    "revision": "2e74b23304caf875c0c41c8fc7cad668",
+    "url": "/clicky-game/static/media/iasip-mac-look.2e74b233.jpg"
+  },
+  {
+    "revision": "a21158c01dca26d330e573cc6aab77e7",
+    "url": "/clicky-game/static/media/iasip-fat-mac.a21158c0.jpg"
+  },
+  {
+    "revision": "13ab54318b8103b4e9621b2ee565fe70",
+    "url": "/clicky-game/static/media/iasip-cricket.13ab5431.jpg"
+  },
+  {
+    "revision": "dce90fe5fe50cc9fc205bb2bf329088b",
+    "url": "/clicky-game/static/media/iasip-dee.dce90fe5.jpg"
+  },
+  {
+    "revision": "a3f469137ac475a276f5c3889a01bf5c",
+    "url": "/clicky-game/static/media/iasip-dennis.a3f46913.jpg"
+  },
+  {
+    "revision": "9512f5bda1b7964551e2c3c228bb5e3f",
+    "url": "/clicky-game/static/media/iasip-frank.9512f5bd.jpg"
+  },
+  {
+    "revision": "9c31ffa32baaf24d1275c5113db8f1cc",
+    "url": "/clicky-game/static/media/iasip-jack.9c31ffa3.jpg"
+  },
+  {
+    "revision": "bbc824b14e5eae48e1d3d632b5838cad",
+    "url": "/clicky-game/static/media/iasip-margret.bbc824b1.jpg"
+  },
+  {
+    "revision": "75da53a82730145992942a6d310d1b3f",
+    "url": "/clicky-game/static/media/iasip-mcpoyles.75da53a8.jpg"
+  },
+  {
+    "revision": "cfd4200905a50fa65962f28fbe2ccf4d",
+    "url": "/clicky-game/static/media/iasip-pondy.cfd42009.jpg"
+  },
+  {
+    "revision": "8f0444e1741aae037c09be52f6516ead",
+    "url": "/clicky-game/index.html"
+  }
+];
